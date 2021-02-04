932da1730ce2329cd015966ca3fb7fc3663c4d78
Jeremy Braun <23155614+jsbrn@users.noreply.github.com>
2021-02-04 12:30:00 -0600

    Finished script

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# autobuy
-The missing piece of the puzzle.
+# passiv-autobuy
+
+[Passiv](https://passiv.com) is a tool that connects to your brokerage and calculates portfolio rebalancing automatically based on targets you select. It then provides a one click BUY button to make the process easier. However, due to strict regulations, they do not expose an API for the executing of orders.
+
+For me, that meant I could not set up my Raspberry Pi to automatically place my preconfigured orders every week. But since I am still able to log in to the website and click the button myself, I decided to write a Python script that will simulate such a thing.
+
+Running this script will connect to your account and execute the orders.
+
+## Setup
+
+It's written for Python 3 but is probably backwards compatible. You'll need to `pip install` the following:
+* `requests`
+* `pyotp`
+* `python-dotenv`
+
+## Running
+
+When you run the script, it will walk you through the process step by step. It will NOT perform any order executions without asking you first, unless you pass `-y` in the arguments.
+
+## Environment Variables
+
+You can supply the required information in a `.env` file to make automation easier:
+
+```
+    EMAIL = "example@example.com"
+    PASSWORD = "examplePassword123"
+    TFA_SECRET = "exAmplESeCr3t"
+```
+
+If you want to enable 2FA on your Passiv account and still run it via a cron job, set up an authenticator app and paste the 2FA secret into the `.env` file. Your authenticator app should have some kind of export function.
---
+++ place_orders.py
@@ -0,0 +1,82 @@
+# Written for Python 3
+
+# Disclaimer: Use this software at your own risk! Make sure your Passiv account is configured in a way that suits you.
+# This script will ONLY list and execute available orders at your discretion. It is equivalent to logging in and clicking the Place Orders button.
+# All orders are calculated on the server according to your account settings.
+
+import requests
+import urllib.request
+import pyotp
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Account Variables
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+TFA_SECRET = os.getenv("TFA_SECRET")
+
+# API Variables
+API_URL = "https://api.passiv.com/api/v1/"
+AUTH_ENDPOINT = "auth/login"
+PORTFOLIO_GROUP_ENDPOINT = "portfolioGroups/"
+PG_INFO_ENDPOINT = 'portfolioGroups/<id>/info'
+IMPACT_ENDPOINT = 'portfolioGroups/<pgid>/calculatedtrades/<ctid>/impact'
+PLACE_ORDERS_ENDPOINT = 'portfolioGroups/<pgid>/calculatedtrades/<ctid>/placeOrders'
+
+# Sign in
+print("Signing in...")
+
+if EMAIL == None:
+    EMAIL = input("Email: ")
+if (PASSWORD == None):
+    PASSWORD = input("Password: ")
+
+session = requests.session()
+login_response = session.post(API_URL + AUTH_ENDPOINT, data={'email': EMAIL, 'password': PASSWORD})
+login_response_json = login_response.json()
+
+jwt_header = {}
+tfa_code = ''
+if 'mfa_required' in login_response_json:
+    if login_response_json['mfa_required']['type'] == 'OTP_TOKEN' and TFA_SECRET != None:
+        print("Generating and submitting TFA code...")
+        tfa_code = pyotp.TOTP(TFA_SECRET).now()
+    else:
+        tfa_code = int(input("Enter your 2FA code: "))
+    tfa_response = session.put(API_URL + AUTH_ENDPOINT, data={'remember_device': False, 'state': login_response_json['mfa_required']['state'], 'token': tfa_code})
+    jwt_header = {'Authorization': 'JWT '+tfa_response.json()['token']}
+else:
+    if 'token' in login_response_json:
+        jwt_header = {'Authorization': 'JWT '+login_response_json.json()['token']}
+
+if jwt_header == {}:
+    sys.exit('Login failed! Please try again.')
+
+print("Success!")
+print("Fetching available orders...")
+
+portfolio_groups_response = session.get(API_URL + "portfolioGroups/", headers=jwt_header)
+groups = portfolio_groups_response.json()
+
+for group in groups:
+    group_info = session.get(API_URL + PG_INFO_ENDPOINT.replace('<id>', group['id']), headers=jwt_header).json()
+    print(group['name']+':')
+    if 'calculated_trades' in group_info:
+        calculated_trades = group_info['calculated_trades']
+        for trade in calculated_trades['trades']:
+            print(' - '+trade['action']+" "+str(trade['units'])+" "+trade['universal_symbol']['symbol']+" @ $"+str(trade['price'])+str(trade['universal_symbol']['currency']['code']))
+        impact = session.get(API_URL + IMPACT_ENDPOINT.replace('<pgid>', group['id']).replace('<ctid>', calculated_trades['id']), headers=jwt_header).json()
+        if 'detail' in impact:
+            print(impact['detail'])
+        else:
+            argument_allows = len(sys.argv) >= 2 and sys.argv[1] == '-y'
+            user_allows = 'y' if argument_allows else input("Execute orders for "+group['name']+"? (y/n): ")
+            if argument_allows or user_allows == 'y':
+                placedOrders = session.post(API_URL + PLACE_ORDERS_ENDPOINT.replace('<pgid>', group['id']).replace('<ctid>', calculated_trades['id']), headers=jwt_header).json()
+                if 'detail' in placedOrders:
+                    print(placedOrders['detail'])
+                else:
+                    print('Orders executed!')
